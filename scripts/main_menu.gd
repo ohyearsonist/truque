e23d172f8ec8511fc13b2f675769898575d66386
Calien <ohyearsonist@gmail.com>
2026-07-24 23:35:08 -0300

    style: change assets
    Change card assets to Kenny's, add fonts, create a basic main menu...

---
+++ scripts/main_menu.gd
@@ -0,0 +1,10 @@
+extends Control
+
+func _on_settings_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_howto_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
